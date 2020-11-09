f92e47e291235d97c2a7dd8be9f51a20ff54b640
John Kim <abcdefghijohnk@gmail.com>
2020-11-09 10:17:14 -0700

    Create num_of_docs and doc_unis methods in hospital model

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,15 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def num_of_docs
+    self.doctors.count
+  end
+
+  def doc_unis
+    uni_array.to_sentence
+  end
+
+  def uni_array
+    self.doctors.select(:university).distinct.pluck(:university)
+  end
 end

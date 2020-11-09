1a2250d4280648b6f7e2f99e7661d3682cc5c829
John Kim <abcdefghijohnk@gmail.com>
2020-11-09 10:15:55 -0700

    create instance methods hospital model spec for num_of_docs and doc_unis methods

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe Hospital, type: :model do
   describe 'relationships' do
     it {should have_many :doctors}
   end
+
+  describe 'instance methods' do
+    before :each do
+      @hospital = Hospital.create(name: "Paiser Al Dente")
+      doc_1 = @hospital.doctors.create(name: "Dr. Bob", specialty: "Pediatrics", university: "UCLA")
+      doc_2 = @hospital.doctors.create(name: "Dr. Jim", specialty: "Orthopedics", university: "UCSD")
+      doc_3 = @hospital.doctors.create(name: "Dr. Joe", specialty: "Neurology", university: "UCLA")
+      doc_3 = @hospital.doctors.create(name: "Dr. Tim", specialty: "Cardiology", university: "UCSF")
+    end
+
+    it '#num_of_docs' do
+      expect(@hospital.num_of_docs).to eq(4)
+    end
+
+    it '#doc_unis' do
+      expect(@hospital.doc_unis).to eq("UCLA, UCSD, and UCSF")
+    end
+  end
 end

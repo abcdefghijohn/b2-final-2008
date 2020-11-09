b0386f680beb9a2f89326b88a8df5042e7acc822
John Kim <abcdefghijohnk@gmail.com>
2020-11-09 09:20:42 -0700

    create feature spec for doctors show page

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+R.describe "As a visitor" do
+  describe "When I visit a doctors show page" do
+    it "I see the doctors name, specialty, university, hospital, and all their patients" do
+      hospital = Hospital.create(name: "Paiser Al Dente")
+      doc = hospital.doctors.create(name: "Dr. Bob", specialty: "Pediatrics", university: "UCLA")
+      sam = doc.patients.create(name: "Sam Bob", age: 4)
+      pam = doc.patients.create(name: "Pam Bob", age: 5)
+      cam = doc.patients.create(name: "Cam Bob", age: 3)
+      visit "/doctors/#{doc.id}"
+
+      expect(page).to have_content(doc.name)
+      expect(page).to have_content(doc.speciality)
+      expect(page).to have_content(doc.university)
+      expect(page).to have_content(doc.hospitals.name)
+      expect(page).to have_content(sam.name)
+      expect(page).to have_content(pam.name)
+      expect(page).to have_content(cam.name)
+    end
+  end
+end
